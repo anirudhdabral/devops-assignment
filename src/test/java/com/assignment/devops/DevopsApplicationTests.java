78aeeaf793c4d5e493c99f42bf7435f2d6401a21
Anirudh Dabral <anirudhdabral49@gmail.com>
2023-12-27 12:14:38 +0530

    update

--- src/test/java/com/assignment/devops/DevopsApplicationTests.java
+++ src/test/java/com/assignment/devops/DevopsApplicationTests.java
@@ -1,6 +1,7 @@
 package com.assignment.devops;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +9,7 @@ class DevopsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
